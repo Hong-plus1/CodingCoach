773f0de191cb5b18f446a12730dbbdd716906993
洪嘉仪 <121852723+Hong-plus1@users.noreply.github.com>
2025-04-17 16:06:17 +0800

    Update main.py
    register

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from coachGenerate import exercise
 from problemCollection import wrongCollection,FavCollection,proSetting
-from login import login,logout
+from login import login,logout,register
 
 st.set_page_config(page_title='编程教练', layout="wide", initial_sidebar_state="expanded")
 
@@ -15,29 +15,36 @@ def temp():
 # 初始化会话状态
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
+if "userid" not in st.session_state:
+    st.session_state.userid = None
+if "show_register" not in st.session_state:
+    st.session_state.show_register = False
 
-login_page = st.Page(login, title="登录")
-logout_page = st.Page(logout, title="退出登录")
-WrongPage=st.Page(wrongCollection,title="错题集")
-FavPage=st.Page(FavCollection,title="收藏集")
-#CodingPage=st.Page("CodeEvaluate.py", title="代码编辑")
-CoachPage=st.Page("coach.py",title="编程教练")
-ChatPage=st.Page("Chat.py",title="答疑解惑")
-CodeExerpage=st.Page(exercise,title="代码练习")
-ProSettingPage=st.Page(proSetting,title="设置")
+if st.session_state.show_register:
+    register()
+else:
+    login_page = st.Page(login, title="登录")
+    logout_page = st.Page(logout, title="退出登录")
+    WrongPage=st.Page(wrongCollection,title="错题集")
+    FavPage=st.Page(FavCollection,title="收藏集")
+    #CodingPage=st.Page("CodeEvaluate.py", title="代码编辑")
+    CoachPage=st.Page("coach.py",title="编程教练")
+    ChatPage=st.Page("Chat.py",title="答疑解惑")
+    CodeExerpage=st.Page(exercise,title="代码练习")
+    ProSettingPage=st.Page(proSetting,title="设置")
 
-pg = st.navigation([login_page])
-if st.session_state.logged_in:    
-    pg = st.navigation(
-        {
-            "主页": [CoachPage],
-            "编程练习": [CodeExerpage,ChatPage],
-            "题目集": [WrongPage,FavPage,ProSettingPage],
-            "账户管理": [logout_page],
-        }
-    )
+    pg = st.navigation([login_page])
+    if st.session_state.logged_in:    
+        pg = st.navigation(
+            {
+                "主页": [CoachPage],
+                "编程练习": [CodeExerpage,ChatPage],
+                "题目集": [WrongPage,FavPage,ProSettingPage],
+                "账户管理": [logout_page],
+            }
+        )
 
-pg.run()
+    pg.run()
 
 
 # addknowledgepoint() 数据库设置
